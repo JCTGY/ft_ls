e593a8d6028b391043bc5bd68199bb7e7ac737d5
Jeffrey  Chiang <jchiang-@e1z3r5p20.42.us.org>
2019-04-06 17:03:53 -0700

    finish flag

--- src/ls_sort.c
+++ src/ls_sort.c
@@ -6,16 +6,35 @@
 /*   By: jchiang- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:16:28 by jchiang-          #+#    #+#             */
-/*   Updated: 2019/04/06 16:27:09 by jchiang-         ###   ########.fr       */
+/*   Updated: 2019/04/06 17:00:26 by jchiang-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+static t_ls		*sort_reverse(t_ls *ls)
+{
+	t_ls	*next;
+	t_ls	*current;
+	t_ls	*prev;
+
+	current = ls;
+	next = NULL;
+	prev = NULL;
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (prev);
+}
+
 int				sort_list(t_ls **begin, int flag)
 {
 	*begin = (!(flag & F_T)) ? sort_alpha(*begin) : *begin;
 	*begin = (flag & F_T) ? sort_time(*begin) : *begin;
-//	(flag & F_LR) && sort_reverse(ls);
+	*begin = (flag & F_LR) ? sort_reverse(*begin) : *begin;
 	return (0);
 }
